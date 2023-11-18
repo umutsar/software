3e97fe6607b0ce4535d8b3d7334263f3fbfdd5b1
Umut Şar <contact@umutsar.net>
2023-11-19 00:02:37 +0300

    Create cikis_sorgula.py

---
+++ Gise-sistemi/cikis_sorgula.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+connection = sqlite3.connect('/U/first_database/first_db')
+
+cursor = connection.cursor()
+
+cursor.execute("SELECT * FROM cikis_bilgileri")
+calisanlar = cursor.fetchall()
+
+print("{:<5} {:<15} {:<15}".format("ID", "Çalışan ID", "Zaman"))
+
+# Ayırıcı satır
+print("="*62)
+
+for calisan in calisanlar:
+    print("{:<5} {:<15} {:<15}".format(calisan[0], calisan[1], calisan[2]))
+
+
+connection.close()
